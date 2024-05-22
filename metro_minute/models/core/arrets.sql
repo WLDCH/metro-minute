49dc616ed2916e54f7c59748cb196214b76745af
WLDCH <wchrimni@gmail.com>
2024-05-22 21:35:16 +0200

    Add transilien to database (i know transilien are not RER)

--- metro_minute/models/core/arrets.sql
+++ metro_minute/models/core/arrets.sql
@@ -2,7 +2,6 @@ SELECT
     ArRId as ar_rid,
     'STIF:StopPoint:Q:' || ArRId || ':' as ar_rref,
     ArRVersion as ar_rversion,
-    -- ArRName as ar_rname,
     {{ clean_stop_name('ArRName') }} as ar_rname,
     ArRType as ar_rtype,
     ArRTown as ar_rtown,
--- metro_minute/models/core/rer_temps_reel.sql
+++ metro_minute/models/core/rer_temps_reel.sql
@@ -1,7 +1,6 @@
 SELECT
     tr.MonitoringRef_ArR as ar_rref,
     {{ clean_stop_name('tr.arrname') }} as ar_rname,
-    -- tr.arrname as ar_rname,
     a.ar_rtown,
     tr.lineref as line_ref,
     tr.name_line,
@@ -10,4 +9,4 @@ SELECT
 FROM {{ source('referentiel_raw', 'donnees_temps_reel') }} tr
 INNER JOIN {{ ref('lignes') }} l ON tr.lineref = l.line_ref
 INNER JOIN {{ ref('arrets') }} a on tr.MonitoringRef_ArR = a.ar_rref
-WHERE l.transport_mode = 'rail' AND tr.name_line IN ('A', 'B', 'C', 'D', 'E')
+WHERE l.transport_mode = 'rail'  AND tr.name_line IN ('A', 'B', 'C', 'D', 'E', 'H', 'J', 'K', 'L', 'N', 'P', 'R')
